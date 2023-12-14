2ed12fb336ad6439356c6d41e823e65833bc8fe1
Tatiana Titova <ritasom666@gmail.com>
2023-12-14 19:21:19 +0400

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+
+
+    public String name;
+    public String surname;
+    public String patronymic;
+    public FormDate birthday;
+    public String phone;
+    public String passport;
+    public boolean subscription;
+
+}
